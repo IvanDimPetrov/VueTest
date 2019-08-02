d84137579db3d6da275f29bcae4d8455a51be70c
Ivan Petrov <ipetrov.cw@mmm.com>
2019-08-02 13:33:49 +0300

    sex commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Мишо е ПИЧ Йохохо Йохохо"/>
+    <HelloWorld msg="Мишо е ПИЧ Йохохо Йохохо sex"/>
   </div>
 </template>
 
